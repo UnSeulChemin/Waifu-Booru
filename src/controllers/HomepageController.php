9a78ba7076b9b7eea22c111801847cd5e58e8084
= <bendev4977@gmail.com>
2023-09-02 01:39:27 +0200

    update

--- src/controllers/HomepageController.php
+++ src/controllers/HomepageController.php
@@ -3,13 +3,6 @@
 // namespace
 namespace Application\Controllers\HomepageController;
 
-// require
-require_once('src/lib/database.php');
-
-// use
-use Application\Lib\Database\DatabaseConnection;
-use Application\Model\Post\PostRepository;
-
 class Homepage
 {
     public function execute()
